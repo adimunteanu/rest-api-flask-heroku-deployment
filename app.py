053ecba9b7474a76924c352f39af6991f14bfcf8
adimunteanu <adimunteanu13@gmail.com>
2021-12-28 14:27:36 +0200

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from db import db
 
 from config import SECRET_KEY
 from security import authenticate, identity
@@ -16,11 +15,6 @@ app.secret_key = SECRET_KEY
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 
@@ -31,5 +25,6 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
